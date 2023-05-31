7d360bdb9b4ac7b23dd341af652cff7ba86a1a09
Belousov Dmitriy <dmitriy@pop-os.localdomain>
2023-05-31 21:50:34 +0500

    Build: message
    * В чате сообщения разделены на свои и чужие.

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -16,6 +16,7 @@ function Chat() {
   const chatRef = useRef<any>()
   const listRef = useRef<any>()
   const stateChat = useSelector((state: any) => state.chatSlice)
+  const {displayName}=useSelector((state: any)=> state.configSlice)
   const [ chatsList, setChatsList ] = useState(stateChat.chatsList)
   const [ top, setTop ] = useState<any>(0)
   useEffect(() => {
@@ -25,6 +26,10 @@ function Chat() {
     setTop(listRef.current?.clientHeight)
     chatRef.current.scrollTop = top
   })
+
+  function messageIsLocal(author: string) {
+    return displayName===author
+  }
   return (<div ref={chatRef} className="chats">
     <List sx={{
       width: '100%',
@@ -37,7 +42,13 @@ function Chat() {
           ref={listRef}
     >
       {chatsList.map((chat: Chat, index: number) => {
-        return (<ListItem key={index} alignItems="flex-start">
+        return (<ListItem sx={{
+            margin: `${messageIsLocal(chat.author)?"10px auto 10px 5px":"10px 5px 10px auto"}`,
+            bgcolor: `${messageIsLocal(chat.author)? 'grey': 'orange'}`,
+            width: "80%",
+            borderRadius: 2,
+
+          }} key={index} alignItems="flex-start">
             <ListItemAvatar>
               <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"/>
             </ListItemAvatar>
