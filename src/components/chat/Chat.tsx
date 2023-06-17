6c2621d3043605cb49b3d8bf76c02d8226b0bd3b
Belousov Dmitriy <dmitriy@pop-os.localdomain>
2023-06-17 22:37:30 +0500

    Fix: typescript правка на Chat компоненте

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, DOMElement, RefObject } from 'react';
 import { useSelector } from "react-redux";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -8,24 +8,22 @@ import Avatar from '@mui/material/Avatar';
 import ChatsMessage from "./ChatsMessage";
 import Glagol from "../../App/Glagol";
 
-
 type Chat = {
   author: string, text: string, id: string
 }
 
 function Chat() {
-  const chatRef = useRef<any>()
-  const listRef = useRef<any>()
+  const chatRef = useRef<HTMLDivElement>(null)
+  const listRef = useRef<HTMLUListElement>(null)
   const stateChat = useSelector((state: any) => state.chatSlice)
-  const { displayName } = useSelector((state: any) => state.configSlice)
   const [chatsList, setChatsList] = useState(stateChat.chatsList)
-  const [top, setTop] = useState<any>(0)
+  const [top, setTop] = useState<number>(0)
   useEffect(() => {
     setChatsList(stateChat.chatsList)
   }, [stateChat])
   useEffect(() => {
-    setTop(listRef.current?.clientHeight)
-    chatRef.current.scrollTop = top
+    if (listRef.current) setTop(listRef.current?.clientHeight)
+    if (chatRef.current !== null) chatRef.current.scrollTop = top
   })
 
   function messageIsLocal(authorId: string) {
@@ -36,7 +34,6 @@ function Chat() {
       width: '100%',
       maxWidth: 360,
       bgcolor: 'rgba(6,6,6,.16)',
-
     }}
       classes={{
         root: 'chats-list'
@@ -46,7 +43,7 @@ function Chat() {
       {chatsList.map((chat: Chat, index: number) => {
         return (<ListItem sx={{
           margin: `${messageIsLocal(chat.id) ? "10px auto 10px 5px" : "10px 5px 10px auto"}`,
-          bgcolor: `${messageIsLocal(chat.id) ? 'grey' : 'orange'}`,
+          bgcolor: `${messageIsLocal(chat.id) ? 'blue' : 'red'}`,
           width: "80%",
           borderRadius: 2,
 
@@ -61,7 +58,6 @@ function Chat() {
             </React.Fragment>}
           />
         </ListItem>
-
         )
       })}
     </List>
